7cec05b1c1a780d6460e427b1b4605341e09ec6e
Valeria Busch Romanazzi <a2018121081@estudiantes.upsa.edu.bo>
2021-03-16 19:04:05 -0400

    LAST

--- FunctionConsumidor/Function1.cs
+++ FunctionConsumidor/Function1.cs
@@ -1,16 +1,17 @@
-using System;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
-using Microsoft.Extensions.Logging;
 
 namespace FunctionConsumidor
 {
+    using Microsoft.Azure.WebJobs;
+    using Microsoft.Extensions.Logging;
+    using Newtonsoft.Json;
+    using FunctionConsumidor.Models;
     public static class Function1
     {
         [FunctionName("Function1")]
         public static void Run([ServiceBusTrigger("cola", Connection = "MyConn")]string myQueueItem, ILogger log)
         {
             log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+            var data = JsonConvert.DeserializeObject<Odometro>(myQueueItem);
         }
     }
 }
--- FunctionConsumidor/Models/Odometro.cs
+++ FunctionConsumidor/Models/Odometro.cs
@@ -1,4 +1,5 @@
-﻿namespace ApiColector.Models
+﻿
+namespace FunctionConsumidor.Models
 {
     using System;
     using System.ComponentModel.DataAnnotations;
@@ -6,14 +7,14 @@
     {
         [Key]
         public string Email { get; set; }
-        
+
         [Required]
         public string Name { get; set; }
-        
+
         [DataType(DataType.DateTime)]
         [Required]
         public DateTime DateTime { get; set; }
-        
+
         [Required]
         public int Step { get; set; }
 
